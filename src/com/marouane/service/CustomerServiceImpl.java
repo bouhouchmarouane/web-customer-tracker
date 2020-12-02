10cfa09fd59711697a5d4d3531be6855e3a92d35
marouanebhch <bouhouch.marouane@gmail.com>
2020-12-02 20:23:31 +0100

    Updating service interface and impl to save customer

--- src/com/marouane/service/CustomerServiceImpl.java
+++ src/com/marouane/service/CustomerServiceImpl.java
@@ -21,4 +21,10 @@ public class CustomerServiceImpl implements CustomerService {
 		return dao.getCustomers();
 	}
 
+	@Override
+	public void saveCustomer(Customer customer) {
+		dao.saveCustomer(customer);
+		
+	}
+
 }
